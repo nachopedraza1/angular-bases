22fb6825038ed51e94016f624685a658d5dcbeeb
nachopedraza1 <nachi_choco@live.com.ar>
2024-01-28 18:16:43 -0300

    curso fer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-bases';
+  public title: string = 'angular-bases';
 }
